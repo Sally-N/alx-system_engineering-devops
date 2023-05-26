03cf1473b4a7e76cfa9c46a8c7037f6d24c4991b
Sally-N <snzungula@gmail.com>
2023-05-26 21:10:41 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
-"""Module returning information about a user's TODO list progress."""
+"""Returns information about a user's TODO list progress."""
 
 import requests
 import sys
 
 
-employee_id = int(sys.argv[1])
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
 
-baseurl = "https://jsonplaceholder.typicode.com/"
-employee_url = f'{baseurl}users/{employee_id}'
-employee_todos_url = f'{baseurl}todos?userId={employee_id}'
+    baseurl = "https://jsonplaceholder.typicode.com/"
+    employee_url = f'{baseurl}users/{employee_id}'
+    employee_todos_url = f'{baseurl}users/{employee_id}/todos'
 
-try:
     # Get employee info
     response = requests.get(employee_url)
-    employee_data = reponse.json()
+    employee_data = response.json()
     employee_name = employee_data['name']
     
 
     # Get employee todos
-    response = requests.get(employee_todos_url)
-    todos_data = response.json()
+    response_todos = requests.get(employee_todos_url)
+    todos_data = response_todos.json()
 
     # Tasks
     total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task['completed']]
-    num_done_tasks = len(done_tasks)
+    done_tasks = 0
+    done_tasks_list = []
+    for task in todos_data:
+        if task.get('completed') is True:
+            done_tasks += 1
+            done_tasks_list.append(task)
 
     # Diplay progress
-    print(f'Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):');
-
+    print(f'Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):');
+    
     # Print completed tasks
-    for task in done_tasks:
+    for task in done_tasks_list:
         print(f"\t{task['title']}")
